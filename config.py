d8a2795b7783bf00e034ba9c34f8bb6845bed3be
Ezequiel Parziale <eparziale@gmail.com>
2022-11-22 20:20:32 -0300

    :wrench: update: upgrade config with pydantic

--- config.py
+++ config.py
@@ -1,29 +1,35 @@
-import os
-from os.path import join, dirname
-from dotenv import load_dotenv
+from pydantic import BaseSettings
+class Settings(BaseSettings):
 
-dotenv_path = join(dirname(__file__), 'kibana.env')
-load_dotenv(dotenv_path)
+    # API KEYS DE TWIITER
+    TWITTER_API_KEY: str
+    TWITTER_API_SECRET_KEY: str
+    TWITTER_ACCESS_TOKEN: str
+    TWITTER_ACCESS_TOKEN_SECRET: str
 
-dotenv_path = join(dirname(__file__), 'twitter_api.env')
-load_dotenv(dotenv_path)
+    # KAFKA
+    SERVER_KAFKA: str = "localhost:9092"
+    TOPIC_NAME: str = "twitter"  # NOMBRE DEL TOPIC DE KAFKA
 
-# API KEYS DE TWIITER
-API_KEY = os.environ.get("TWITTER_API_KEY")
-API_SECRET_KEY = os.environ.get("TWITTER_API_SECRET_KEY")
-ACCESS_TOKEN = os.environ.get("TWITTER_ACCESS_TOKEN")
-ACCESS_TOKEN_SECRET = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
+    # ELASTIC
+    ELASTIC_SERVER: str = 'localhost:9200'
+    ELASTICSEARCH_USERNAME: str
+    ELASTICSEARCH_PASSWORD: str
 
-# KAFKA
-SERVER_KAFKA = 'localhost:9092'
-TOPIC_NAME = 'twitter' # NOMBRE DEL TOPIC DE KAFKA
+    # CONFIGURACION PARA BUSQUEDA DE TUITS
+    TRACKS: list = [
+        "#argentina",
+        "argentina",
+        "seleccion",
+        "messi",
+        "escaloneta",
+        "afa",
+    ]
+    LOCATION: list = [-126.2, -56.0, 22.3, 58.9]
+    LANGUAGES: list = ["en", "es"]
 
-# ELASTIC
-ELASTIC_SERVER = 'localhost:9200'
-ELASTIC_USER = os.environ.get("ELASTICSEARCH_USERNAME")
-ELASTIC_PASS = os.environ.get("ELASTICSEARCH_PASSWORD")
+    class Config:
+        env_file = [".env"]
 
-# CONFIGURACION PARA BUSQUEDA DE TUITS
-TRACKS = ['#argentina','argentina','seleccion','messi','escaloneta','afa']
-LOCATION = [-126.2,-56.0,22.3,58.9]
-LANGUAGES = ['en','es']
+
+settings = Settings()
